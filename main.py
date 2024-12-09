481ef585c7e6b29f81f5849a89b8ae687b0337ae
Yan <tal531764@gmail.com>
2024-12-10 01:18:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-a = input("Введите строку: ")
-count = 0
-for i in a:
-    if "и" == i.lower():
+a = input("Введите строку: ") #Вводим строку
+count = 0 #Счетчик
+for i in a: #Перебор
+    if "и" == i.lower(): #Условие
         count += 1
-print("Буква и встречается: " + str(count) + " раз")
+print("Буква и встречается: " + str(count) + " раз") #Вывод
